ff0e6c95a245ad92eb48c4dd7bbc3d26f3611115
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2021-07-27 12:24:55 -0500

    included insert into mongodb adding test files

--- src/main/java/org/learn/WordCounter.java
+++ src/main/java/org/learn/WordCounter.java
@@ -4,7 +4,6 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
 
